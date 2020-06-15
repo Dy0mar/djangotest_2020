36803916e3b67c88097847cabc2f4f3616a27ec5
Dy0mar <kurtudins@gmail.com>
2020-06-15 10:26:26 +0300

    add notes and db

--- djangotest_2020/settings.py
+++ djangotest_2020/settings.py
@@ -75,6 +75,17 @@ WSGI_APPLICATION = 'djangotest_2020.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'django_db',
+#         'USER': 'user_name',
+#         'PASSWORD': 'password',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
